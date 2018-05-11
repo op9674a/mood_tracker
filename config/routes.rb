7cb574d1719d8753c53a3c104865d0bfa83c1b67
Olena Penn <olenapenn@gmail.com>
2018-05-10 18:53:09 -1000

    login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+      collection do
+        post '/login', to: 'users#login'
+      end
+    end
 
   get '/records', to: 'records#index'
   get '/records/:id', to: 'records#show'
